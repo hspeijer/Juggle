51475063a61a76cd4cbba2b9631cf693c2d77271
Hans Speijer <hans.speijer@mindsteps.eu>
2017-04-06 22:02:25 +0200

    Yup

---
+++ Assets/Force.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Force : MonoBehaviour
+{
+	public float thrust;
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(new Vector3(5,20,5) * thrust);
+	}
+
+	void FixedUpdate()
+	{
+	}
+}
